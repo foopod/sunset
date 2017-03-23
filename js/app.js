6ebb68f2c2518a88364f726d4928af494df0a921
Jono Shields <jonathon.shields@vodafone.com>
2017-03-24 08:30:26 +1300

    minor twral

--- js/app.js
+++ js/app.js
@@ -16,7 +16,8 @@ var rangeHeights;
 var mountainColor;
 
 function gameLoop() {
-    drawGradient(red, blue);
+//    drawGradient(red, blue);
+    drawGradient(red, blendColors("#FFFFFF", red, 0.5));
     drawSun(sunColor, sunX, sunY, sunR);
 //    drawClouds(count); //Lags out
     for(var i = 0; i < rangeHeights.length; i++){
@@ -24,7 +25,7 @@ function gameLoop() {
         drawMountainRange(rangeHeights[i],fluctuation,count*(i+2));
         } else {
             drawTreeRange(rangeHeights[i],fluctuation,count*(i+2));
-            drawTreeRange(rangeHeights[i]+0.03,fluctuation,count*(i+2)-10);
+            drawTreeRange(rangeHeights[i]+0.03  ,fluctuation,count*(i+3)-12);
          }
     }
 //    drawMountainRange(0.5,fluctuation,count);
@@ -126,7 +127,7 @@ ctx.lineTo(i,perlin.noise((i+step+1)/200,height*canvas.height,10)*terrainFluctat
     ctx.lineWidth = 1;
 //    ctx.strokeStyle = 'blue';
     var grd=ctx.createLinearGradient(0,canvas.height*height,0,canvas.height);
-    grd.addColorStop(0,shadeColor2(mountainColor, (1/height-0.3)-1));
+    grd.addColorStop(0,shadeColor2(mountainColor, (1/height-0.5)-1));
     console.log((height*2)-.7);
     var grdColor  = 160-(Math.floor(255*sunY/canvas.height));
 //    grd.addColorStop(1, "".concat('rgb(',grdColor,',',grdColor,',',grdColor,')'));
@@ -141,7 +142,7 @@ function drawTreeRange(height, fluctuation, step){
     ctx.beginPath();
     ctx.moveTo(0, canvas.height);
     for(var i = 0; i <= canvas.width; i+=canvas.width/numberOfPoints){
-        ctx.lineTo(i,octavePerlin((i+step)/200,height*canvas.height*10,1, 10, 0.5)*terrainFluctation*canvas.height*2 + Math.abs(10 - (i+step)%20) * 5 + height*canvas.height*1);
+        ctx.lineTo(i,octavePerlin((i+step)/200,height*canvas.height*10,1, 10, 0.5)*terrainFluctation*canvas.height*2 + Math.abs(10 - (i+step)%20) * 5 + height*canvas.height*1.05);
     }
 
 
